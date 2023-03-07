f190e7ccfb0ade9b54e22f71cbe31f8ff2585636
洮羱芝闇 <shun_@outlook.com>
2023-03-07 17:18:36 +0800

    update

--- 作业/课上作业/20230307/src/计算商品总价.java
+++ 作业/课上作业/20230307/src/计算商品总价.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class 计算商品总价 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a=0;
+        int n=0;
+        System.out.print("请输入商品单价（元）：");
+        a = input.nextDouble();
+        System.out.print("请输入商品数量：");
+        n = input.nextInt();
+        System.out.println("商品的总价是："+a*n+"元");
+    }
 }
---
+++ 作业/课上作业/20230307/src/计算学员总成绩及平均分.java
@@ -0,0 +1,5 @@
+public class 计算学员总成绩及平均分 {
+    public static void main(String[] args) {
+        
+    }
+}
