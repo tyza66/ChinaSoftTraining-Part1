f7fa4eac12e8e050faa4e0200fc1110d6d3f68f6
洮羱芝闇 <shun_@outlook.com>
2023-03-07 12:41:32 +0800

    update

---
+++ 代码/ideaworkspace/javase01/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+        //快速生成main方法  psvm 或者 main 回车
+        //输出语句 sout+回车
+        //单行注释
+        System.out.println("hello world");
+    }
+}
