59a27d9e15cb91e82c0c3b8954581b189182481d
洮羱芝闇 <shun_@outlook.com>
2023-03-07 16:16:27 +0800

    update

---
+++ 练习/IDEAWorkSpace/javase01/src/Test05.java
@@ -0,0 +1,9 @@
+public class Test05 {
+    public static void main(String[] args) {
+        //三目运算符
+        int a=1,b=2;
+        System.out.println(a>b?"a大于b":"a小于等于b");
+        int x=9,y=8;
+        System.out.println(x>y?x+"大于"+y:x+"小于等于"+y);
+    }
+}
