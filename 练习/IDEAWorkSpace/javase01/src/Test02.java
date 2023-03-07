345ff8c38b26db38d2a9cf5be89f09ce7e7290f4
洮羱芝闇 <shun_@outlook.com>
2023-03-07 13:26:36 +0800

    update

---
+++ 练习/IDEAWorkSpace/javase01/src/Test02.java
@@ -0,0 +1,19 @@
+public class Test02 {
+    public static void main(String[] args) {
+        byte a = 123;
+        short b = 456;
+        int c = 789;
+        long d = 666;
+
+        float e = 0.1f;
+        double f = 0.02;
+
+        char g = 'g';
+
+        boolean h = false;
+
+        String i = "good";
+
+        System.out.println(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" "+h+" "+i);
+    }
+}
