da7d5d17586a1724627f48925a85af1eba1983f0
洮羱芝闇 <shun_@outlook.com>
2023-03-08 08:56:11 +0800

    update

---
+++ 练习/IDEAWorkSpace/javase02/src/Test01.java
@@ -0,0 +1,12 @@
+public class Test01 {
+    public static void main(String[] args) {
+        int i = 20;
+        i+=1;
+        i-=2;
+        i*=2;
+        i/=3;
+        i%=2;
+        i+=1;
+        System.out.println(i);
+    }
+}
