fb7e44dd9b526968645be87644899d1871241e9a
洮羱芝闇 <shun_@outlook.com>
2023-03-10 11:21:05 +0800

    update

--- 作业/javaoop中的作业/src/文件作业/使用mkdir完成mkdirs的功能.java
+++ 作业/javaoop中的作业/src/文件作业/使用mkdir完成mkdirs的功能.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class 使用mkdir完成mkdirs的功能 {
     public static void main(String[] args) {
-        String path = "C:\\Users\\shun_\\Desktop\\Project\\Public\\ChinaSoft\\作业\\javaoop中的作业\\src\\文件作业\\我是一个新新的文件夹\\giao2";
+        String path = "C:\\Users\\shun_\\Desktop\\Project\\Public\\ChinaSoft\\作业\\javaoop中的作业\\src\\文件作业\\wangwang\\wang";
         File f = new File(path);
         //f.mkdir();
         //f.mkdirs();
