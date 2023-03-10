0218e86c259c14b6e9e0818a92e8fc7078ada467
洮羱芝闇 <shun_@outlook.com>
2023-03-10 13:10:47 +0800

    update

--- 作业/javaoop中的作业/src/IO流作业/IOCopy.java
+++ 作业/javaoop中的作业/src/IO流作业/IOCopy.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class IOCopy {
     public static void main(String[] args) {
         try {
+            //整个读出来的方式
             FileInputStream fis = new FileInputStream("C:\\Users\\shun_\\Desktop\\Project\\Public\\ChinaSoft\\作业\\javaoop中的作业\\src\\IO流作业\\giao.txt");
             byte[] a = new byte[64];
             fis.read(a);
@@ -12,6 +13,7 @@ public class IOCopy {
             FileOutputStream out = new FileOutputStream("C:\\Users\\shun_\\Desktop\\Project\\Public\\ChinaSoft\\作业\\javaoop中的作业\\src\\IO流作业\\giao2.txt");
             out.write(a);
             FileOutputStream out2 = new FileOutputStream("C:\\Users\\shun_\\Desktop\\Project\\Public\\ChinaSoft\\作业\\javaoop中的作业\\src\\IO流作业\\giao3.txt",true);
+            //边读边写的方式
             fis = new FileInputStream("C:\\Users\\shun_\\Desktop\\Project\\Public\\ChinaSoft\\作业\\javaoop中的作业\\src\\IO流作业\\giao.txt");
             int data=0;
             while((data=fis.read())!=-1){
