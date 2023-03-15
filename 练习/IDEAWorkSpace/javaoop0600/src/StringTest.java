b2a7107a1c42e9d0410f087763882346f7350d8b
洮羱芝闇 <shun_@outlook.com>
2023-03-15 15:14:16 +0800

    update

--- 练习/IDEAWorkSpace/javaoop0600/src/StringTest.java
+++ 练习/IDEAWorkSpace/javaoop0600/src/StringTest.java
@@ -3,5 +3,8 @@ public class StringTest {
         String str1 = "giao";
         boolean flag = str1.contains("o");
         System.out.println(flag);
+        String str2 = new String("shenqi");
+        String str3 = str2.replaceFirst("qi","giao");
+        System.out.println(str3);
     }
 }
