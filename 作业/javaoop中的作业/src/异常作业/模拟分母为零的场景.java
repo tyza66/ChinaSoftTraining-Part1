fc92e8ae9ba55d30c2665828dd66c743e7d52528
洮羱芝闇 <shun_@outlook.com>
2023-03-17 14:54:13 +0800

    update

--- 作业/javaoop中的作业/src/异常作业/模拟分母为零的场景.java
+++ 作业/javaoop中的作业/src/异常作业/模拟分母为零的场景.java
@@ -3,16 +3,13 @@ package 异常作业;
 public class 模拟分母为零的场景 {
     public static void main(String[] args) {
         int i = 0;
-        try {
-            for (i = -10; i <= 10; i++) {
+        for (i = -10; i <= 10; i++) {
+            try {
                 int b = 10 / i;
+            } catch (Exception e) {
+                i = i + 1;
             }
-        } catch (Exception e) {
-            System.out.println("你遇到了一个异常，但是以为被catch了，程序不会因此而终止");
-            for (i = i+1; i <= 10; i++) {
-                int b = 10 / i;
-            }
-            System.out.println("执行完了");
         }
+        System.out.println(i);
     }
 }
