652b46b8d21f1169a6ee559903451f143fe75d04
洮羱芝闇 <shun_@outlook.com>
2023-03-19 11:43:25 +0800

    update

--- 练习/IDEAWorkSpace/javaoop0800/src/Test.java
+++ 练习/IDEAWorkSpace/javaoop0800/src/Test.java
@@ -1,22 +1,25 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Solution.convert("ABCD", 2));
+        System.out.println(Solution.convert("Apalindromeisaword,phrase,number,orothersequenceofunitsthatcanbereadthesamewayineitherdirection,withgeneralallowancesforadjustmentstopunctuationandworddividers.", 3));
     }
 }
 
 class Solution {
     public static String convert(String s, int numRows) {
         if (numRows == 1) return s;
-        String[][] out = new String[numRows][s.length() / numRows + (numRows - 1) * numRows];
+        char[][] out = new char[numRows][s.length() / numRows + numRows * numRows + 99];
         int i = 0, j = 0;
-        String end = "";
-        //0是直线向下模式
-        //1是斜线向右上角走模式
         int mode = 0;
         for (String one : s.split("")) {
-            if (i < 0 || j < 0) break;
+            if (i < 0) {
+                i = 0;
+            }
             if (mode == 0) {
-                out[i][j] = one;
+                try {
+                    out[i][j] = one.charAt(0);
+                } catch (Exception e) {
+                    System.out.println(i + "" + j);
+                }
                 i++;
                 if (i == numRows) {
                     i = i - 2;
@@ -24,18 +27,24 @@ class Solution {
                     mode = 1;
                 }
             } else if (mode == 1) {
-                out[i][j] = one;
-                    i--;
-                    j++;
-                if (i == -1) {
+                try {
+                    out[i][j] = one.charAt(0);
+                } catch (Exception e) {
+                    System.out.println(i + "" + j);
+                }
+                if (i == 0) {
                     i++;
                     mode = 0;
+                } else {
+                    j++;
+                    i--;
                 }
             }
         }
-        for (String[] i1 : out) {
-            for (String j1 : i1) {
-                if (j1 != null) {
+        String end = "";
+        for (char[] i1 : out) {
+            for (char j1 : i1) {
+                if (j1 != '\u0000') {
                     end = end + j1;
                 }
             }
