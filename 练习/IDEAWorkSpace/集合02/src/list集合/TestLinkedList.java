b29dcbb5a80a8f9d148aa9682bbff795094057ea
洮羱芝闇 <shun_@outlook.com>
2023-03-21 09:05:15 +0800

    update

--- 练习/IDEAWorkSpace/集合02/src/list集合/TestLinkedList.java
+++ 练习/IDEAWorkSpace/集合02/src/list集合/TestLinkedList.java
@@ -1,5 +1,6 @@
 package list集合;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class TestLinkedList {
@@ -7,7 +8,7 @@ public class TestLinkedList {
         //底层是基于双向链表实现的数据结构 有很多的队列表头部和尾部的操作方法
         LinkedList<Integer> lst = new LinkedList<>();
         //添加元素
-        for (int i=1;i<=11;i++){
+        for (int i = 1; i <= 11; i++) {
             lst.add(i);
         }
         System.out.println(lst);
@@ -17,8 +18,47 @@ public class TestLinkedList {
         lst.addLast(11);
         System.out.println(lst);
         //返回列表中的第一个元素
-        System.out.println("头部元素是:"+lst.getFirst());
+        System.out.println("头部元素是:" + lst.getFirst());
         //返回列表中的尾部元素
-        System.out.println("尾部元素是:"+lst.getLast());
+        System.out.println("尾部元素是:" + lst.getLast());
+        //添加元素
+        lst.offerFirst(-1);
+        //往列表尾部添加元素
+        lst.offerLast(12);
+        System.out.println("添加后：" + lst);
+        //获取并移除第一个元素
+        System.out.println("拉出头部元素：" + lst.pollFirst());
+        //获取并移除最后一个元素
+        System.out.println("拉出尾部元素：" + lst.pollLast());
+        System.out.println(lst);
+        //获取并不删除头部元素
+        System.out.println("获取并不移除头部元素：" + lst.peekFirst());
+        //获取并不移除尾部元素
+        System.out.println("获取并不移除尾部元素：" + lst.peekLast());
+        //从列表中弹出一个元素
+        System.out.println("弹出一个元素：" + lst.pop());
+        System.out.println("再弹出一个元素：" + lst.pop());
+        System.out.println();
+        //往列表中推入一个元素
+        lst.push(1);
+        System.out.println(lst);
+
+        lst.push(0);
+        System.out.println(lst);
+        //集合循环遍历
+        for (int i = 0; i < lst.size(); i++) {
+            Integer ele = lst.get(i);
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+
+        for (Integer ele : lst) {
+            System.out.print(ele + " ");
+        }
+        Iterator<Integer> it = lst.iterator();
+        while(it.hasNext()){
+            Integer ele = it.next();
+            System.out.print(ele + " ");
+        }
     }
 }
