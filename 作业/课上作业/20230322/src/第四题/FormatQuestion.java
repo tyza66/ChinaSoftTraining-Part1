b2ad6beda71458dbfbf8baa5fde2c34df3e93368
洮羱芝闇 <shun_@outlook.com>
2023-03-22 15:20:42 +0800

    update

--- 作业/课上作业/20230322/src/第四题/FormatQuestion.java
+++ 作业/课上作业/20230322/src/第四题/FormatQuestion.java
@@ -1,4 +1,12 @@
 package 第四题;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class FormatQuestion {
+    public static void main(String[] args) throws FileNotFoundException {
+        BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+
+    }
 }
