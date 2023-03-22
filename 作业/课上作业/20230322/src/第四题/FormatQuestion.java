fe7cc5e7ed2387751d322f515964f775379d46ef
洮羱芝闇 <shun_@outlook.com>
2023-03-22 16:38:13 +0800

    update

--- 作业/课上作业/20230322/src/第四题/FormatQuestion.java
+++ 作业/课上作业/20230322/src/第四题/FormatQuestion.java
@@ -1,12 +1,41 @@
 package 第四题;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
 
 public class FormatQuestion {
-    public static void main(String[] args) throws FileNotFoundException {
-        BufferedReader br = new BufferedReader(new FileReader("test.txt"));
-
+    public static void main(String[] args) throws IOException {
+        Question[] qs = parseFile("C:\\Users\\shun_\\Desktop\\Project\\Public\\ChinaSoft\\作业\\课上作业\\20230322\\驾驶员测试题.txt");
+        for(Question q  : qs){
+            q.show();
+        }
+    }
+    public static Question[] parseFile(String fileName) throws IOException {
+        File file = new File(fileName);
+        InputStreamReader isr = new InputStreamReader(new FileInputStream(file), "GBK");
+        BufferedReader br = new BufferedReader(isr);
+        ArrayList<Question> out = new ArrayList();
+        //6行一组，快乐读取不可以 因为后面有陷阱 发现后面有俩选项的
+        //备选方案就是通过每一行第一个字符是啥来决定了
+        String line = "";
+        String question = "", answer = "";
+        String[] options = new String[4];
+        int opitionsNum = 0;
+        while ((line = br.readLine()) != null) {
+            if (line.charAt(0) >= '0' && line.charAt(0) <= '9') {
+                question = line;
+            } else if (line.length()!=1&&line.charAt(0) >= 'A' && line.charAt(0) <= 'D') {
+                options[opitionsNum] = line;
+                opitionsNum++;
+            } else if (line.length()==1) {
+                answer = line;
+                String[] endop = new String[opitionsNum];
+                System.arraycopy(options, 0, endop, 0 ,opitionsNum);
+                //new Question(question, endop, answer).show();
+                out.add(new Question(question, endop, answer));
+                opitionsNum = 0;
+            }
+        }
+        return out.toArray(new Question[0]);
     }
 }
--- 作业/课上作业/20230322/src/第四题/Question.java
+++ 作业/课上作业/20230322/src/第四题/Question.java
@@ -2,12 +2,21 @@ package 第四题;
 
 public class Question {
     String question;
-    String[] options = new String[4];
-    int answer;
+    String[] options;
+    String answer;
+
+    public Question() {
+    }
+
+    public Question(String question, String[] options, String answer) {
+        this.question = question;
+        this.options = options;
+        this.answer = answer;
+    }
 
     public void show() {
         System.out.println(question);
-        for (int i = 0; i <= 3; i++) {
+        for (int i = 0; i <= options.length-1; i++) {
             System.out.println(options[i]);
         }
         System.out.println("答案：" + answer);
@@ -18,10 +27,10 @@ public class Question {
     }
 
     public void getOpition(int i) {
-        if (i >= 1 && i <= 4) {
+        if (i >= 1 && i <= answer.length()) {
             System.out.println(options[i-1]);
         } else {
-            System.out.println("你需要输入1-4的数字获得对应的选项");
+            System.out.println("你需要输入1-"+answer.length()+"的数字获得对应的选项");
         }
     }
 }
