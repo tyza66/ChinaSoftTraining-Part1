3bc41e18bcf39cd4cace0434ca1e85574672ea34
洮羱芝闇 <shun_@outlook.com>
2023-03-24 12:47:24 +0800

    update

---
+++ 作业/课上作业/20230324/src/第六题/GBKtoUTF8.java
@@ -0,0 +1,27 @@
+package 第六题;
+
+import java.io.*;
+
+public class GBKtoUTF8 {
+    public static void main(String[] args) {
+        String old = "C:\\Users\\shun_\\Desktop\\Project\\Public\\ChinaSoft\\作业\\课上作业\\20230324\\编码是GBK的文件.txt";
+        String newPath="C:\\Users\\shun_\\Desktop\\Project\\Public\\ChinaSoft\\作业\\课上作业\\20230324";
+        try {
+            change(old,newPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void change(String old,String newOnePath) throws IOException {
+        File i0 = new File(old);
+        File i1 = new File(newOnePath+"\\utf8-"+i0.getName());
+        InputStreamReader s1 = new InputStreamReader(new FileInputStream(i0), "GBK");
+        OutputStreamWriter s2 = new OutputStreamWriter(new FileOutputStream(i1), "UTF-8");
+        int temp = 0;
+        while((temp=s1.read())!=-1){
+            s2.write(temp);
+        }
+        s1.close();
+        s2.close();
+    }
+}
