8b07922499f861a27d52d6e8dd79bce9b84a7f6e
洮羱芝闇 <shun_@outlook.com>
2023-03-24 10:35:09 +0800

    update

--- 作业/课上作业/20230324/src/第二题/DirCut.java
+++ 作业/课上作业/20230324/src/第二题/DirCut.java
@@ -6,12 +6,27 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
 public class DirCut {
-    //文件件的剪切
-    public void dirCutDown(String old,String end) throws FileNotFoundException {
-        //获得要剪切的原始文件夹和要输出的目标文件夹上级目录位置
-        FileInputStream in = new FileInputStream(new File(old));
-        FileOutputStream out = new FileOutputStream(new File(end));
-        //先复制目录结构
-        
+    public static void main(String[] args) {
+
     }
+    //文件夹的剪切
+    public void dirCopyDown(String old,String end) throws FileNotFoundException {
+        //文件夹是一个树的结构其实 我们要走到树干复制树干（文件夹） 走到果子复制果子（文件）
+        //获得要剪切的原始文件夹和要输出的目标文件夹上级目录或者文件位置
+        File fold =new File(old);
+        File fend =new File(end);
+        FileInputStream in = new FileInputStream(fold);
+        FileOutputStream out = new FileOutputStream(fend);
+        if(fold.isDirectory()){
+            new File(end+"\\"+fold.getName()).mkdir();
+            for(File f:fold.listFiles()){
+                dirCopyDown(f.getAbsolutePath(),end+"\\"+fold.getName());
+            }
+        }else{
+
+        }
+
+    }
+
+
 }
