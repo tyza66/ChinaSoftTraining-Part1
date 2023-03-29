10d0bf9fc7f0bc5004c9fa3656cb3bde0c9c6978
洮羱芝闇 <shun_@outlook.com>
2023-03-30 07:43:50 +0800

    update

--- 阶段项目/项目开发团队分配管理软件/src/com/team/view/managements/DeveloperManagementMenu.java
+++ 阶段项目/项目开发团队分配管理软件/src/com/team/view/managements/DeveloperManagementMenu.java
@@ -1,6 +1,7 @@
 package com.team.view.managements;
 
 import com.team.service.common.Loading;
+import com.team.service.common.Step;
 import com.team.service.managements.DeveloperManagement;
 
 import java.util.Scanner;
@@ -31,7 +32,7 @@ public class DeveloperManagementMenu {
             System.out.println("0.返回上级菜单");
             System.out.print("输入指令：");
             String code = input.next();
-            switch(code){
+            switch (code) {
                 case "1":
                     addOne();
                     break;
@@ -45,43 +46,74 @@ public class DeveloperManagementMenu {
                     break;
             }
         }
-
+        Step.longNext();
     }
-    public void addOne(){
+
+    public void addOne() {
         Scanner input = new Scanner(System.in);
-        String id,name,age,salary,bouns,stock;
+        String id, name, age, salary, bouns, stock;
         System.out.println("你要添加的开发人员类型是：");
         System.out.println("1.程序员");
         System.out.println("2.设计师");
         System.out.println("3.架构师");
         String code = input.next();
-        if(code.equals("1")){
-            System.out.print("请输入id：");
-            id = input.next();
-            System.out.print("请输入name：");
-            name = input.next();
-            System.out.print("请输入age：");
-            age = input.next();
-            System.out.print("请输入salary：");
-            salary = input.next();
-        }else if(code.equals("2")){
-            System.out.print("请输入id：");
-            System.out.print("请输入name：");
-            System.out.print("请输入age：");
-            System.out.print("请输入salary：");
-            System.out.print("请输入bonus：");
-        }else if(code.equals("3")){
-            System.out.print("请输入id：");
-            System.out.print("请输入name：");
-            System.out.print("请输入age：");
-            System.out.print("请输入salary：");
-            System.out.print("请输入bonus：");
-            System.out.print("请输入stock：");
-        }else {
+        if (code.equals("1")) {
+            try {
+                System.out.print("请输入id：");
+                id = input.next();
+                System.out.print("请输入name：");
+                name = input.next();
+                System.out.print("请输入age：");
+                age = input.next();
+                System.out.print("请输入salary：");
+                salary = input.next();
+                dm.add(Integer.parseInt(id), name, Integer.parseInt(age), Double.parseDouble(salary), null);
+                System.out.println("添加成功！");
+            } catch (Exception e) {
+                System.out.println("您输入的信息有误，添加失败！");
+            }
+        } else if (code.equals("2")) {
+            try {
+                System.out.print("请输入id：");
+                id = input.next();
+                System.out.print("请输入name：");
+                name = input.next();
+                System.out.print("请输入age：");
+                age = input.next();
+                System.out.print("请输入salary：");
+                salary = input.next();
+                System.out.print("请输入bonus：");
+                bouns = input.next();
+                dm.add(Integer.parseInt(id), name, Integer.parseInt(age), Double.parseDouble(salary), null, Double.parseDouble(bouns));
+                System.out.println("添加成功！");
+            } catch (Exception e) {
+                System.out.println("您输入的信息有误，添加失败！");
+            }
+        } else if (code.equals("3")) {
+            try {
+                System.out.print("请输入id：");
+                id = input.next();
+                System.out.print("请输入name：");
+                name = input.next();
+                System.out.print("请输入age：");
+                age = input.next();
+                System.out.print("请输入salary：");
+                salary = input.next();
+                System.out.print("请输入bonus：");
+                bouns = input.next();
+                System.out.print("请输入stock：");
+                stock = input.next();
+                dm.add(Integer.parseInt(id), name, Integer.parseInt(age), Double.parseDouble(salary), null, Double.parseDouble(bouns), Integer.parseInt(stock));
+                System.out.println("添加成功！");
+            } catch (Exception e) {
+                System.out.println("您输入的信息有误，添加失败！");
+            }
+        } else {
             System.out.println("您输入的信息有误！");
         }
     }
-    public void showAll(){
+
+    public void showAll() {
 
     }
 }
