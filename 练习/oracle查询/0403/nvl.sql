968e8ecd437ba1fcd696dade6dccaed5aee54a2c
洮羱芝闇 <shun_@outlook.com>
2023-04-03 17:11:15 +0800

    update

--- 练习/oracle查询/0403/nvl.sql
+++ 练习/oracle查询/0403/nvl.sql
@@ -1,2 +1,11 @@
 select empno,ename,nvl(comm,0)+100 from emp;
+
 select empno||'giao'||ename from emp;
+select * from emp;
+SELECT 1+1 from dual;
+SELECT DISTINCT * from emp;
+SELECT * FROM emp WHERE comm is not null;
+SELECT * from emp where DEPTNO^=10;
+SELECT * from emp where DEPTNO!=10;
+SELECT * from emp where DEPTNO<>10;
+SELECT * from emp WHERE ENAME LIKE '%\。%' ESCAPE '\';
