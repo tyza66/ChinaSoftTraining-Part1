d421e3cb8619628e7055836b27b2735d50a9a8c7
洮羱芝闇 <shun_@outlook.com>
2023-04-04 08:57:54 +0800

    update

--- 练习/oracle查询/0403/nvl.sql
+++ 练习/oracle查询/0403/nvl.sql
@@ -1,5 +1,5 @@
-select empno,ename,nvl(comm,0)+100 from emp;
-
+select empno,ename,nvl(comm,0)+100 from emp; 
+--如果当第一个参数值为空时自动选择参数2，如果不为空还是参数1
 select empno||'giao'||ename from emp;
 select * from emp;
 SELECT 1+1 from dual;
