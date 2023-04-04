5b179dfd639ab8f9b54525d1601e957ffb5da4a9
洮羱芝闇 <shun_@outlook.com>
2023-04-04 10:16:12 +0800

    update

--- 练习/oracle查询/0404/order.sql
+++ 练习/oracle查询/0404/order.sql
@@ -4,3 +4,8 @@ select ename,sal,sal*12 yearpay from emp where sal*12>10000;
 select ename,sal,sal*12 yearpay from emp order by yearpay asc;
 select ename,sal,sal*12 yearpay from emp order by yearpay desc;
 --排序里面可以别名也可以用计算后的
+select ename,lower(ename) lowername,upper(ename) uppername,initcap(ename) initcapname from emp;
+select ename,substr(ename,2,2) from emp;
+select ename,substr(ename,2) from emp;
+SQL> select ename,instr(ename,'A') from emp;
+select ename,instr(ename,'A'), instr(ename,'A',2) from emp;
