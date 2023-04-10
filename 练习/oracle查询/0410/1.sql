2ec4dd3f698a71db1ea6337667ef227c076724e6
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-10 09:58:24 +0800

    update

--- 练习/oracle查询/0410/1.sql
+++ 练习/oracle查询/0410/1.sql
@@ -1,4 +1,10 @@
 select NULLIF(1,2) from dual;
 select COALESCE(NULL, null, null, 2) FROM dual;
 SELECT DEPTNO,ENAME,SAL,DECODE(DEPTNO, 10,nvl(sal,0)+100,20,nvl(sal,0)+200,30,nvl(sal,0)+300,nvl(sal,0)+1) newsal from emp ORDER BY SAL;
-SELECT DEPTNO,ENAME,SAL,DECODE(DEPTNO, 10,nvl(sal,0)+100,20,nvl(sal,0)+200,30,nvl(sal,0)+300,nvl(sal,0)+1) as newsal1,case when depton=10 then nvl(sal,0)+100 when deptno=20 then nvl(sal,0)+200 when deptno=30 then nvl(sal,0)+300 else nvl(sal,0)+1 end as newsal2 from emp ORDER BY SAL;
+SELECT DEPTNO,ENAME,SAL,DECODE(DEPTNO, 10,nvl(sal,0)+100,20,nvl(sal,0)+200,30,nvl(sal,0)+300,nvl(sal,0)+1) as newsal1,case when deptno=10 then nvl(sal,0)+100 when deptno=20 then nvl(sal,0)+200 when deptno=30 then nvl(sal,0)+300 else nvl(sal,0)+1 end as newsal2 from emp ORDER BY deptno;
+-- 查询员工姓名及其对应的部门编号
+select e.ename,d.dname from emp e,dept d where e.DEPTNO = d.DEPTNO;
+--查询salgrade表中的内容
+select * from salgrade;
+--查询emp表的薪资等级在哪个区间里面
+select e.ename,e.sal,s.grade from emp e,salgrade s where e.sal between s.LOSAL and HISAL;
