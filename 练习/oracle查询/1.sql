72bd45511da4ceaa07547233622221594157361d
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-11 09:25:37 +0800

    update

--- 练习/oracle查询/1.sql
+++ 练习/oracle查询/1.sql
@@ -26,3 +26,18 @@ select ename,sal from emp where sal<=all(select sal from emp where ename = 'ALLE
 
 select ename,sal from emp where sal in (select max(sal) from emp group by deptno);
 
+select max(sal) from emp group by deptno;
+select ename,sal from emp order by sal desc;
+
+select ename,sal from emp where sal is not null order by sal desc;
+
+select max(sal) from emp where sal is not null group by deptno;
+
+-- 大于最大的
+select ename,sal from emp where sal >= all(select max(sal) from emp where sal is not null group by deptno);
+-- 大于任意一个
+select ename,sal from emp where sal >= any (select max(sal) from emp where sal is not null group by deptno);
+-- 小于最小的
+select ename,sal from emp where sal <= all (select max(sal) from emp where sal is not null group by deptno);
+-- 小于任意一个 就是小于最大的
+select ename ,sal from emp where sal< any (select max(sal) from emp where sal is not null group by deptno);
