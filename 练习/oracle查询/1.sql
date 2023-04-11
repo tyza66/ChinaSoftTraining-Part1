0c07a09e7693a7b148c41946067a713acf23c24f
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-11 09:14:11 +0800

    udpate

--- 练习/oracle查询/1.sql
+++ 练习/oracle查询/1.sql
@@ -1,6 +1,5 @@
 select deptno,sum(nvl(sal,0)) from emp GROUP BY deptno;
 
-
 select
 	e.deptno,d.dname,count(*),sum(nvl(e.sal,0)) sumsal
 from 
@@ -11,3 +10,19 @@ group by
 	e.deptno,d.dname
 having
 	sum(nvl(e.sal,0))>10000;
+	
+select ename,sal from emp where ename = 'ALLEN';
+
+SELECT ename,sal from emp order by sal desc;
+
+select ename,sal from emp where sal>(select sal from emp where ename ='ALLEN');
+
+select ename,sal from emp where deptno = 10;
+
+select sal from emp where ename = 'ALLEN' or ename = 'SMITH';
+
+select ename,sal from emp where sal>all(select sal from emp where ename = 'ALLEN' or ename = 'SMITH');
+select ename,sal from emp where sal<=all(select sal from emp where ename = 'ALLEN' or ename = 'SMITH');
+
+select ename,sal from emp where sal in (select max(sal) from emp group by deptno);
+
