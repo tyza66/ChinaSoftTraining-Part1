17ec9e7ff27a65ba9c2a14e27d5832740a3b2db7
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-04-17 09:56:14 +0800

    update

--- 代码/0417/jdbcdemo/src/com/mypack/test/Test4.java
+++ 代码/0417/jdbcdemo/src/com/mypack/test/Test4.java
@@ -2,9 +2,7 @@ package com.mypack.test;
 
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Test4 {
 
@@ -43,6 +41,7 @@ public class Test4 {
 
 
     }
+    //commit  rollback 转账
 
     @Test
     public void testUpdateOne(){
@@ -86,5 +85,100 @@ public class Test4 {
         }
 
 
+    }
+
+    @Test
+    public void updateMoney(){
+        String driverClassName="oracle.jdbc.OracleDriver";
+        String username="scott";
+        String password="tiger";
+        String url="jdbc:oracle:thin:@localhost:1521:orcl";
+        String sql1="update emp set sal=sal-500 where ename='ALLEN'";
+        String sql2="update emp set sal=sal+500 where ename='SMITH'";
+
+        Connection connection=null;
+        Statement stmt=null;
+
+        try {
+            Class.forName(driverClassName);
+            connection= DriverManager.getConnection(url, username, password);
+            //开启手动提交事务 默认是自动提交
+            connection.setAutoCommit(false);
+
+            stmt = connection.createStatement();
+            stmt.executeUpdate(sql1);
+            stmt.executeUpdate(sql2);
+            connection.commit();//手动提交事务  如果执行到这一行，说明没报异常
+        }catch(Exception e){
+            e.printStackTrace();
+            try {
+                if (connection != null) {
+                    connection.rollback();//事务的回滚
+                }
+            }catch (Exception e1){
+                e1.printStackTrace();
+            }
+        }finally {
+            try {
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Test
+    public void testGetCount(){
+        String driverClassName="oracle.jdbc.OracleDriver";
+        String username="scott";
+        String password="tiger";
+        String url="jdbc:oracle:thin:@localhost:1521:orcl";
+        String sql="select count(*) from stu";
+
+        Connection conn=null;
+        Statement stmt=null;
+        ResultSet rs = null;
+
+        try {
+            //1. 注册驱动
+            Class.forName(driverClassName);
+            //2. 获取连接
+            conn = DriverManager.getConnection(url, username, password);
+            //如果不为null，说明能连上oracle数据库
+            System.out.println(conn);
+            //   3. 创建Statement对象   找一辆车
+            stmt = conn.createStatement();
+            //4. 执行操作  executeQuery() 执行查询的操作
+            rs = stmt.executeQuery(sql);
+            //5. 处理结果集
+           if(rs.next()){
+               int count = rs.getInt(1);
+               System.out.println("一共有"+count+"条记录");
+           }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            //6. 关闭资源 最近使用先关
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+
+        }
     }
 }
---
+++ 练习/oracle查询/jdbcDemo/src/Test4.java
@@ -0,0 +1,101 @@
+import org.junit.Test;
+
+import java.sql.*;
+
+/**
+ * Author: tyza66
+ * Date: 2023/04/17 9:40
+ * Github: https://github.com/tyza66
+ */
+public class Test4 {
+    @Test
+    public void testUpdateMoney(){
+        updateMoney();
+    }
+    @Test
+    public void testGetCount(){
+        getCount();
+    }
+    //通过事务的形式进行操作
+    public void updateMoney(){
+        Connection conn = null;
+        String driverClassName = "oracle.jdbc.OracleDriver";
+        String user = "scott";
+        String password = "tiger";
+        String url = "jdbc:oracle:thin:@localhost:1521:helowin";
+        String sql1 = "update emp set sal= sal-500 where ename = 'ALLEN'";
+        String sql2 = "update emp set sal= sal+500 where ename = 'SMITH'";
+        try {
+            Class.forName(driverClassName);
+            conn = DriverManager.getConnection(url, user, password);
+            //开启手动提交事务 默认是自动的
+            conn.setAutoCommit(false);
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate(sql1);
+            stmt.executeUpdate(sql2);
+            //手动提交事务 如果执行到了这一行 就说明没有报异常
+            conn.commit();
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            if (conn != null) {
+                try {
+                    conn.rollback();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+        }finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //获得多少条记录
+    public void getCount(){
+        Connection conn = null;
+        String driverClassName = "oracle.jdbc.OracleDriver";
+        String user = "scott";
+        String password = "tiger";
+        String url = "jdbc:oracle:thin:@localhost:1521:helowin";
+        String sql = "select count(*) from emp";
+        try {
+            Class.forName(driverClassName);
+            conn = DriverManager.getConnection(url, user, password);
+            //开启手动提交事务 默认是自动的
+            Statement stmt = conn.createStatement();
+            ResultSet resultSet = stmt.executeQuery(sql);
+            while (resultSet.next()) {
+                int rows = resultSet.getInt(1);
+                System.out.println("表中一共有"+rows+"条数据");
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            if (conn != null) {
+                try {
+                    conn.rollback();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+        }finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+        }
+    }
+}
